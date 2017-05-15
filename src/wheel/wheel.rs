17c6e66e6fdcb509021ff043a520c6412ad8e58f
Bick Doner <bdoner@users.noreply.github.com>
2017-05-15 22:23:39 +0200

    type confusion

--- src/wheel/wheel.rs
+++ src/wheel/wheel.rs
@@ -4,11 +4,11 @@ use self::rand::{thread_rng, Rng};
 pub fn spin() -> String {
     let mut rng = thread_rng();
     let punishments = vec! {
-        "death by the crazy 88s",
-        "death by surprise jihad",
-        "death by cantrymen",
-        "forced to program in ArnoldC for a living",
-        "dot|not hits you with a wrench",
+        format!("death by the crazy 88s"),
+        format!("death by surprise jihad"),
+        format!("death by cantrymen"),
+        format!("forced to program in ArnoldC for a living"),
+        format!("dot|not hits you with a wrench"),
         format!("gulag {} years", rng.gen_range(0, 1_000_000))
     };    
     let i = rng.gen_range(0, punishments.len());
